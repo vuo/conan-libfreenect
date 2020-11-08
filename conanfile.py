8702af23fc01fd4a9281d769c563a4cec89eb0ae
Steve Mokris <steve@kosada.com>
2020-11-08 10:50:51 -0500

    Update to libfreenect 0.6.1 (latest).  Build for both x86_64 + arm64.  [kosada/vuo/vuo#17836]

--- conanfile.py
+++ conanfile.py
@@ -1,23 +1,29 @@
 from conans import ConanFile, CMake, tools
+import os
 import platform
 import shutil
 
 class LibfreenectConan(ConanFile):
     name = 'libfreenect'
 
-    source_version = '0.5.6'
-    package_version = '3'
+    source_version = '0.6.1'
+    package_version = '0'
     version = '%s-%s' % (source_version, package_version)
 
-    build_requires = 'llvm/3.3-5@vuo/stable'
-    requires = 'libusb/1.0.21-3@vuo/stable'
+    build_requires = (
+        'llvm/5.0.2-1@vuo/stable',
+        'macos-sdk/11.0-0@vuo/stable',
+    )
+    requires = 'libusb/1.0.23-0@vuo/stable'
     settings = 'os', 'compiler', 'build_type', 'arch'
-    url = 'https://github.com/vuo/conan-libfreenect'
+    url = 'https://github.com/OpenKinect/libfreenect'
     license = 'https://github.com/OpenKinect/libfreenect/blob/master/APACHE20'
     description = 'Driver for the Kinect for Windows v1 / Kinect for Xbox 360'
     source_dir = 'libfreenect-%s' % source_version
-    build_dir = '_build'
     generators = 'cmake'
+
+    build_dir = '_build'
+    install_dir = '_install'
 
     def requirements(self):
         if platform.system() == 'Linux':
@@ -27,7 +33,7 @@ class LibfreenectConan(ConanFile):
 
     def source(self):
         tools.get('https://github.com/OpenKinect/libfreenect/archive/v%s.tar.gz' % self.source_version,
-                  sha256='5ec1973cd01fd864f4c5ccc84536aa2636d0be768ba8b1c2d99026f3cd1abfd3')
+                  sha256='a2e426cf42d9289b054115876ec39502a1144bc782608900363a0c38056b6345')
 
         tools.replace_in_file('%s/CMakeLists.txt' % self.source_dir,
                               'PROJECT(libfreenect)',
@@ -48,36 +54,42 @@ class LibfreenectConan(ConanFile):
                               ''')
 
     def build(self):
+        cmake = CMake(self)
+        cmake.definitions['BUILD_AS3_SERVER'] = False
+        cmake.definitions['BUILD_CPP'] = False
+        cmake.definitions['BUILD_CV'] = False
+        cmake.definitions['BUILD_C_SYNC'] = False
+        cmake.definitions['BUILD_EXAMPLES'] = False
+        cmake.definitions['BUILD_FAKENECT'] = False
+        cmake.definitions['BUILD_OPENNI2_DRIVER'] = False
+        cmake.definitions['BUILD_PYTHON'] = False
+        cmake.definitions['BUILD_PYTHON2'] = False
+        cmake.definitions['BUILD_PYTHON3'] = False
+        cmake.definitions['BUILD_REDIST_PACKAGE'] = True
+        cmake.definitions['CONAN_DISABLE_CHECK_COMPILER'] = True
+        cmake.definitions['CMAKE_BUILD_TYPE'] = 'Release'
+        cmake.definitions['CMAKE_C_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang'
+        cmake.definitions['CMAKE_C_FLAGS'] = cmake.definitions['CMAKE_CXX_FLAGS'] = '-Oz'
+        cmake.definitions['CMAKE_INSTALL_PREFIX'] = '%s/%s' % (os.getcwd(), self.install_dir)
+        if platform.system() == 'Darwin':
+            cmake.definitions['CMAKE_OSX_ARCHITECTURES'] = 'x86_64;arm64'
+            cmake.definitions['CMAKE_OSX_DEPLOYMENT_TARGET'] = '10.11'
+            cmake.definitions['CMAKE_OSX_SYSROOT'] = self.deps_cpp_info['macos-sdk'].rootpath
+        cmake.definitions['CMAKE_CXX_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang++'
+
         tools.mkdir(self.build_dir)
         with tools.chdir(self.build_dir):
-            cmake = CMake(self)
-            cmake.definitions['BUILD_AS3_SERVER'] = False
-            cmake.definitions['BUILD_CPP'] = False
-            cmake.definitions['BUILD_CV'] = False
-            cmake.definitions['BUILD_C_SYNC'] = False
-            cmake.definitions['BUILD_EXAMPLES'] = False
-            cmake.definitions['BUILD_FAKENECT'] = False
-            cmake.definitions['BUILD_OPENNI2_DRIVER'] = False
-            cmake.definitions['BUILD_PYTHON'] = False
-            cmake.definitions['BUILD_PYTHON2'] = False
-            cmake.definitions['BUILD_PYTHON3'] = False
-            cmake.definitions['BUILD_REDIST_PACKAGE'] = True
-            cmake.definitions['CMAKE_C_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang'
-            cmake.definitions['CMAKE_C_FLAGS'] = cmake.definitions['CMAKE_CXX_FLAGS'] = '-Oz'
-            if platform.system() == 'Darwin':
-                cmake.definitions['CMAKE_C_FLAGS'] += ' -mmacosx-version-min=10.10'
-                cmake.definitions['CMAKE_CXX_FLAGS'] += ' -mmacosx-version-min=10.10'
-            cmake.definitions['CMAKE_CXX_COMPILER'] = self.deps_cpp_info['llvm'].rootpath + '/bin/clang++'
             cmake.configure(source_dir='../%s' % self.source_dir,
                             build_dir='.')
             cmake.build()
+            cmake.install()
 
-            # They forgot to update the version number.
+        with tools.chdir(self.install_dir):
             if platform.system() == 'Darwin':
-                shutil.move('lib/libfreenect.0.5.5.dylib', 'lib/libfreenect.dylib')
+                shutil.move('lib/libfreenect.%s.dylib' % self.source_version, 'lib/libfreenect.dylib')
                 self.run('install_name_tool -id @rpath/libfreenect.dylib lib/libfreenect.dylib')
             elif platform.system() == 'Linux':
-                shutil.move('lib/libfreenect.so.0.5.5', 'lib/libfreenect.so')
+                shutil.move('lib/libfreenect.so.%s' % self.source_version, 'lib/libfreenect.so')
                 patchelf = self.deps_cpp_info['patchelf'].rootpath + '/bin/patchelf'
                 self.run('%s --set-soname libfreenect.so lib/libfreenect.so' % patchelf)
 
@@ -89,8 +101,8 @@ class LibfreenectConan(ConanFile):
         else:
             raise Exception('Unknown platform "%s"' % platform.system())
 
-        self.copy('*.h', src='%s/include' % self.source_dir, dst='include/libfreenect')
-        self.copy('libfreenect.%s' % libext, src='%s/lib' % self.build_dir, dst='lib')
+        self.copy('*.h', src='%s/include' % self.install_dir, dst='include')
+        self.copy('libfreenect.%s' % libext, src='%s/lib' % self.install_dir, dst='lib')
 
         self.copy('%s.txt' % self.name, src=self.source_dir, dst='license')
 
